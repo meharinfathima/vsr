bc3d25b066e1beb506884a978d8239904219fec8
meharinfathima <fmeharin1@gmail.com>
2025-08-20 16:00:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from './components/Hero'
+
+const App = () => {
+  return (
+    <div className='font-sans'>
+      <Hero />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='text-secondary font-sans p-8 text-center'>
+        <h1 className="text-4xl font-bold animate-fadeIn bg-gradient-to-r from-primary 
+        via-black to-primary bg-clip-text text-transparent">
+  Silent Speech Recognition for the Voiceless
+</h1>
+
+    </div>
+  )
+}
+
+export default Hero
